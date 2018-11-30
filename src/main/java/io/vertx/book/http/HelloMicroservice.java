25bb29f71ec2939de8a805e9eab423fa3572c0db
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2018-11-30 15:47:48 -0600

    Routing and handlers with methods

--- src/main/java/io/vertx/book/http/HelloMicroservice.java
+++ src/main/java/io/vertx/book/http/HelloMicroservice.java
@@ -1,7 +1,10 @@
 package io.vertx.book.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class HelloMicroservice extends AbstractVerticle {
 
@@ -9,9 +12,19 @@ public class HelloMicroservice extends AbstractVerticle {
   public void start() {
     //Routing and parameters
     Router router = Router.router(vertx);
-    router.get("/").handler(rc -> rc.response().end("Hello"));
-    router.get("/:name").handler(rc-> rc.response().end("Hello" + rc.pathParam("name")));
+    // Map each with own handler
+    router.get("/").handler(this::hello);
+    router.get("/:name").handler(this::hello);
     vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+  }
+
+  private void hello(RoutingContext rc) {
+    String message = "Hello";
+    if (rc.pathParam("name") != null) {
+      message += " " + rc.pathParam("name");
+    }
+    JsonObject json = new JsonObject().put("message", message);
+    rc.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json").end(json.encode());
   }
 
 }
