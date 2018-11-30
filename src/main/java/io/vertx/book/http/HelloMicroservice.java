cab1b18578e98709f1d622db7089b7590a844c62
Eduardo C. Flores Ambrosio <efloresambrosio@gmail.com>
2018-11-30 15:21:22 -0600

    Routes and parameters on Vertx
    Routes and parameters on Vertx

--- src/main/java/io/vertx/book/http/HelloMicroservice.java
+++ src/main/java/io/vertx/book/http/HelloMicroservice.java
@@ -1,14 +1,17 @@
 package io.vertx.book.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class HelloMicroservice extends AbstractVerticle {
 
   @Override
   public void start() {
-    vertx.createHttpServer().requestHandler(req -> req.response()
-        .end())
-        .listen(8080);
+    //Routing and parameters
+    Router router = Router.router(vertx);
+    router.get("/").handler(rc -> rc.response().end("Hello"));
+    router.get("/:name").handler(rc-> rc.response().end("Hello" + rc.pathParam("name")));
+    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
   }
 
 }
